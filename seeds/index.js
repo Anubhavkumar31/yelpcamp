4e9fb12a600ca96bca0b11016bce8ebbfbd60b5a
Anubhav <anubhav.kumar1@icloud.com>
2024-04-29 02:49:30 +0530

    testing2

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground')
-
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl)
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
